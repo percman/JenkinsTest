b0bdd297ada4ed590b074b4a708de4e8daf69713
aaalfaro <andyalfaro91@yahoo.com>
2018-04-21 00:37:41 -0400

    Removed commented code

--- Week_1/Project0/java/Application.java
+++ Week_1/Project0/java/Application.java
@@ -1,10 +1,8 @@
 package com.revature.project_0;
 
 public class Application {
-	
+
 	public static void main(String[] args) {
 		Login.menu();
-//		UserList list = new UserList();
-//		SerializationOfNewAccount.SerializeList(list);
 	}
 }
