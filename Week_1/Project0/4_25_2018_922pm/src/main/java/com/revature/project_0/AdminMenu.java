1f9adf17b8245bf28ddbe0f81046c7650a0c86fb
Jesse S <jesseskipper14@my.whitworth.edu>
2018-04-25 21:24:25 -0400

    Fixed all scanners, minor other fixes

---
+++ Week_1/Project0/4_25_2018_922pm/src/main/java/com/revature/project_0/AdminMenu.java
@@ -0,0 +1,409 @@
+package com.revature.project_0;
+
+import java.util.ConcurrentModificationException;
+import java.util.Scanner;
+
+public class AdminMenu {
+
+	/*public static void menu(MainAdmin admin) {
+		System.out.println("Main Admin");
+	} under construction*/ 
+
+	// All of the admins options are here
+	public static void menu(User admin) {
+		System.out.println("\n\n\n\n\nAdministrator priviledges detected. Special Access and permissions granted.\n");
+		//Scanner input = new Scanner(System.in);
+
+		String choice = "";
+		while (choice != "1" && choice != "2" && choice != "3" && choice != "4" && choice != "9") {
+			System.out.println("======================");
+			System.out.println("  ADMINISTRATOR MENU");
+			System.out.println("======================");
+			System.out.println("To view all existing users\t\tEnter 1");
+			System.out.println("To view pending account requests (" + ListOfUsers.numberOfPending() + ")\tEnter 2");
+			System.out.println("To view total statement\t\t\tEnter 3");
+			System.out.println("To view all transactions\t\tEnter 4");
+			System.out.println("For user menu\t\t\t\tEnter 5");
+			System.out.println("\n\n\t\tTo log out\t\tEnter 9");
+			System.out.println("\n\n\n\n\n\n\n\n\n\n");
+			choice = Initialization.input.next();
+			System.out.println(""); // whitespace
+			switch (choice) {
+			case "1": { // View all accounts
+				optionOne(admin);
+				choice = "0";
+				break;
+			}
+			case "2": { // View all pending account requests
+				optionTwo(admin);
+				choice = "0";
+				break;
+			}
+			case "3": { // View total balance
+				System.out.println("=========================");
+				System.out.println("  CURRENT TOTAL BALANCE");
+				System.out.println("=========================");
+				double totalBalance = 0;
+				for (User user : ListOfUsers.users) {
+					totalBalance += Double.parseDouble(user.getAccountBalance());
+				}
+				System.out.println("Current bank balance: $" + totalBalance);
+				System.out.println("Session transactions total: " + "UNIMPLEMENTED");
+				System.out.println();
+			}
+			case "4": { // View total transactions/all transactions
+				System.out.println("Not implemented");
+				break;
+			}
+			case "5": { // let the admin access his or her user menu
+				Initialization.logger.trace("User: " + admin.getFirstName() + " " + admin.getLastName() + " logged in.");
+				UserMenu.menu(admin);
+				break;
+			}
+			default: { // Ensure correct entries
+				System.out.println("\n\nPlease enter a valid menu option\n\n");
+			}
+			case "9": { // Log out
+				System.out.println("Successfully logged out. Bye " + admin.getFirstName());
+				Initialization.logger.trace("Admin: " + admin.getFirstName() + " " + admin.getLastName() + " logged out.");
+				return;
+			}
+
+			}
+
+		}
+		// Secret menu option
+	}
+
+	// SubMenu of option 1
+	public static void optionOne(User admin) {
+
+		//Scanner input = new Scanner(System.in);
+		String choice = "";
+		while (choice != "1" && choice != "2" && choice != "3" && choice != "4" && choice != "9") {
+			System.out.println("==================");
+			System.out.println("  EXISTING USERS");
+			System.out.println("==================");
+			for (User user : ListOfUsers.users) {
+				if (user.isApproved() == 2) {
+					System.out.println((ListOfUsers.users.indexOf(user) + 1) + ". " + user.toString());
+				}
+			}
+			System.out.println("===================");
+			System.out.println("  ACCOUNT OPTIONS");
+			System.out.println("===================");
+			System.out.println("To lock an account, Enter 1 followed by the number of the"
+					+ " account you want to lock (eg 1 4 to lock account 4)");
+			System.out.println("To unlock an account, Enter 2 followed by the number of the"
+					+ " account you want to unlock (eg 2 4 to unlock account 4)");
+			System.out.println("To delete an account, Enter 3 followed by the number of the"
+					+ " account you want to delete (eg 3 2 to delete account 2)");
+			System.out.println("To promote an account to admin status, Enter 4 followed by the number of the"
+					+ " account you want to promote (eg 4 5 to promote account 5)");
+			System.out.println("To demote an account to user status, Enter 5 followed by the number of the"
+					+ " account you want to demote (eg 5 3 to demote account 3)");
+			System.out.println("\n\t\tTo return to main admin menu\t Enter 9\n\n");
+			choice = Initialization.input.next();
+			switch (choice) {
+			case "1": { // Lock a single account
+				choice = Initialization.input.nextLine();
+				try {
+					if (ListOfUsers.users.indexOf(admin) + 1 == Integer.parseInt(choice.replaceAll(" ", ""))) {
+						System.out.println("You cannot lock yourself!"); // Don't let an admin lock themselves
+					} else if (1 == Integer.parseInt(choice.replaceAll(" ", ""))) {
+						System.out.println("You cannot lock the main admin"); // Don't allow changes to the main admin!
+					} else {
+						ListOfUsers.users.get(Integer.parseInt(choice.replaceAll(" ", "")) - 1).lock();
+						System.out.println("Account locked!"); // Lock user
+						Initialization.logger.trace("Administator: " + admin.getFirstName() + " " + admin.getLastName() + " locked an account.");
+					}
+					break;
+
+				} catch (NumberFormatException nfe) { // This will detect if a non integer is input by the user
+					System.out.println("Incorrect entry detected. You must input 2 numbers seperated by a space");
+					Initialization.logger.info("Entry error");
+					break;
+				} catch (IndexOutOfBoundsException ioobe) { // This will detect if the user tries to access an index not
+															// on the list
+					System.out.println("\nThat user does not exist!\n\n");
+					Initialization.logger.info("Entry error");
+					break;
+				}
+			}
+			case "2": { // Unlock a single account
+				choice = Initialization.input.nextLine();
+				try {
+					if (ListOfUsers.users.indexOf(admin) + 1 == Integer.parseInt(choice.replaceAll(" ", ""))) {
+						System.out.println("You cannot modify yourself"); // Don't let an admin attempt to unlock
+																			// themselves
+					} else if (1 == Integer.parseInt(choice.replaceAll(" ", ""))) {
+						System.out.println("You cannot modify the main admin"); // Don't allow changes to the main
+																				// admin!
+					} else {
+						ListOfUsers.users.get(Integer.parseInt(choice.replaceAll(" ", "")) - 1).unlock();
+						System.out.println("Account unlocked!"); // Unlock user
+						Initialization.logger.trace("Administator: " + admin.getFirstName() + " " + admin.getLastName() + " unlocked an account.");
+					}
+					break;
+
+				} catch (NumberFormatException nfe) { // This will detect if a non integer is input by the user
+					System.out.println("Incorrect entry detected. You must input 2 numbers seperated by a space");
+					Initialization.logger.info("Entry error");
+					break;
+				} catch (IndexOutOfBoundsException ioobe) { // This will detect if the user tries to access an index not
+															// on the list
+					System.out.println("\nThat user does not exist!\n\n");
+					Initialization.logger.info("Entry error");
+					break;
+				}
+
+			}
+			case "3": { // Delete an account
+				choice = Initialization.input.nextLine();
+				try {
+					if (ListOfUsers.users.indexOf(admin) == 0) { // only give this access to the main admin
+						if (ListOfUsers.users.indexOf(admin) + 1 == Integer.parseInt(choice.replaceAll(" ", ""))) {
+							System.out.println("You cannot delete yourself!"); // Don't allow changes to the main admin
+						} else {
+							ListOfUsers.users
+									.remove(ListOfUsers.users.get(Integer.parseInt(choice.replaceAll(" ", "")) - 1));
+							System.out.println("Account deleted!"); // Delete user
+							ListOfUsers.setNumberOfUsers(ListOfUsers.getNumberOfUsers() - 1);
+							Initialization.logger.trace("Administator: " + admin.getFirstName() + " " + admin.getLastName() + " deleted an account.");
+						}
+						break;
+					} else {
+						System.out.println("\n\n\t\t***YOU DO NOT HAVE ACCESS TO THIS FEATURE***");
+						Initialization.logger.info("Administator: " + admin.getFirstName() + " " + admin.getLastName() + " denied access.");
+						break;
+					}
+				} catch (NumberFormatException nfe) { // This will detect if a non integer is input by the user
+					System.out.println("Incorrect entry detected. You must input 2 numbers seperated by a space");
+					Initialization.logger.info("Entry error");
+					break;
+				} catch (IndexOutOfBoundsException ioobe) { // This will detect if the user tries to access an index not
+															// on the list
+					System.out.println("\nThat user does not exist!\n\n");
+					Initialization.logger.info("Entry error");
+					break;
+				}
+			}
+			case "4": { // Promote an account to admin
+				choice = Initialization.input.nextLine();
+				try {
+					if (ListOfUsers.users.indexOf(admin) == 0) { // Only give this access to the main admin
+						if (ListOfUsers.users.indexOf(admin) + 1 == Integer.parseInt(choice.replaceAll(" ", ""))) {
+							System.out.println("You are already an Admin!"); // Don't allow changes to the main admin
+						} else {
+							ListOfUsers.users.get(Integer.parseInt(choice.replaceAll(" ", "")) - 1).promoteAdmin();
+							System.out.println("Account promoted to Admin!"); // Unlock user
+							Initialization.logger.trace("Administator: " + admin.getFirstName() + " " + admin.getLastName() + " promoted an account.");
+						}
+						break;
+					} else {
+						System.out.println("\n\n\t\t***YOU DO NOT HAVE ACCESS TO THIS FEATURE***");
+						Initialization.logger.info("Administator: " + admin.getFirstName() + " " + admin.getLastName() + " denied access.");
+						break;
+					}
+				} catch (NumberFormatException nfe) { // This will detect if a non integer is input by the user
+					System.out.println("Incorrect entry detected. You must input 2 numbers seperated by a space");
+					Initialization.logger.info("Entry error");
+					break;
+				} catch (IndexOutOfBoundsException ioobe) { // This will detect if the user tries to access an index not
+															// on the list
+					System.out.println("\nThat user does not exist!\n\n");
+					Initialization.logger.info("Entry error");
+					break;
+				}
+			}
+			case "5": { // Demote an admin
+				choice = Initialization.input.nextLine();
+				try {
+					if (ListOfUsers.users.indexOf(admin) == 0) { // Only give this access to the main admin
+						if (ListOfUsers.users.indexOf(admin) + 1 == Integer.parseInt(choice.replaceAll(" ", ""))) {
+							System.out.println("You cannot demote yourself!"); // Don't allow changes to the main admin
+						} else {
+							ListOfUsers.users.get(Integer.parseInt(choice.replaceAll(" ", "")) - 1).demoteAdmin();
+							System.out.println("Account demoted to user!"); // Unlock user
+							Initialization.logger.trace("Administator: " + admin.getFirstName() + " " + admin.getLastName() + " demoted an account.");
+						}
+						break;
+					} else {
+						System.out.println("\n\n\t\t***YOU DO NOT HAVE ACCESS TO THIS FEATURE***");
+						Initialization.logger.info("Administator: " + admin.getFirstName() + " " + admin.getLastName() + " denied access.");
+						break;
+					}
+				} catch (NumberFormatException nfe) { // This will detect if a non integer is input by the user
+					System.out.println("Incorrect entry detected. You must input 2 numbers seperated by a space");
+					Initialization.logger.info("Entry error");
+					break;
+				} catch (IndexOutOfBoundsException ioobe) { // This will detect if the user tries to access an index not
+															// on the list
+					System.out.println("\nThat user does not exist!\n\n");
+					Initialization.logger.info("Entry error");
+					break;
+				}
+			}
+			default: { // catch entry errors
+				System.out.println("\n\nPlease enter a valid menu option");
+				break;
+			}
+			case "9": { // return to main admin menu
+				return;
+			}
+			}
+
+		}
+	}
+
+	// SubMenu of optionTwo
+	public static void optionTwo(User admin) {
+
+		String choice = "";
+		while (choice != "1" && choice != "2" && choice != "3" && choice != "4" && choice != "9") {
+			System.out.println("====================");
+			System.out.println("  PENDING ACCOUNTS");
+			System.out.println("====================");
+			for (User user : ListOfUsers.users) {
+				if (user.isApproved() == 1) {
+					System.out.println((ListOfUsers.users.indexOf(user) + 1) + ". " + user.toString());
+				}
+			}
+			System.out.println("===================");
+			System.out.println("  ACCOUNT OPTIONS");
+			System.out.println("===================");
+			System.out.println("To approve a single account, Enter 1 followed by the number of the "
+					+ "account that you wish to approve (eg 1 7 to approve account 7)");
+			System.out.println("To approve all accounts, Enter 2");
+			System.out.println("To deny a single account, Enter 3 followed by the number of the "
+					+ "account that you wish to approve (eg 3 4 to deny account)");
+			System.out.println("To deny all accounts, Enter 4");
+			System.out.println("You view denied accounts, Enter 5");
+			System.out.println("To permanently delete all denied accounts, Enter 6");
+			System.out.println("To restore denied accounts back to pending, Enter 7");
+			System.out.println("\n\t\tTo return to administrator menu, Enter 9\n\n\n\n");
+			choice = Initialization.input.next();
+			switch (choice) {
+			case "1": { // Approve a single account
+				choice = Initialization.input.nextLine();
+				try {
+					if (ListOfUsers.users.indexOf(admin) + 1 == Integer.parseInt(choice.replaceAll(" ", ""))) {
+						System.out.println("You are already approved!\n\n"); // Don't allow changes to the main admin!
+					} else { // Check for an already approved account
+						if (ListOfUsers.users.get(Integer.parseInt(choice.replaceAll(" ", "")) - 1).isApproved() == 2) {
+							System.out.println("That account is already approved!");
+						} else { // approve the account
+							ListOfUsers.users.get(Integer.parseInt(choice.replaceAll(" ", "")) - 1).approve();
+							System.out.println("Account approved!\n\n");
+							Initialization.logger.trace("Administator: " + admin.getFirstName() + " " + admin.getLastName() + " approved an account.");
+						}
+					}
+					break;
+
+				} catch (NumberFormatException nfe) {
+					System.out.println("Incorrect entry detected. You must input 2 numbers. Spacing does not matter");
+					Initialization.logger.info("Entry error");
+					break;
+				} catch (IndexOutOfBoundsException ioobe) {
+					System.out.println("\nThat user does not exist!\n\n");
+					Initialization.logger.info("Entry error");
+					break;
+				}
+			}
+			case "2": { // Approve all pending accounts
+				for (User user : ListOfUsers.users) {
+					if (user.isApproved() == 1) {
+						user.approve();
+					}
+				}
+				Initialization.logger.trace("Administator: " + admin.getFirstName() + " " + admin.getLastName() + " approved all accounts.");
+				System.out.println("Pending accounts approved!");
+				break;
+			}
+			case "3": { // Deny a single account
+				choice = Initialization.input.nextLine();
+				try {
+					if (ListOfUsers.users.indexOf(admin) + 1 == Integer.parseInt(choice.replaceAll(" ", ""))) {
+						System.out.println("You are already approved! You cannot deny your own account!\n\n");
+					} else { // Check that the account is eligible for denial
+						if (ListOfUsers.users.get(Integer.parseInt(choice.replaceAll(" ", "")) - 1).isApproved() == 2) {
+							System.out.println("This account has been approved and is not eligible for denial");
+						} else { // Deny an account
+							ListOfUsers.users.get(Integer.parseInt(choice.replaceAll(" ", "")) - 1).deny();
+							System.out.println("Account denied!\n\n");
+							Initialization.logger.trace("Administator: " + admin.getFirstName() + " " + admin.getLastName() + " denied an account.");
+						}
+					}
+					break;
+
+				} catch (NumberFormatException nfe) {
+					System.out.println("Incorrect entry detected. You must input 2 numbers. Spacing does not matter");
+					Initialization.logger.info("Entry error");
+					break;
+				} catch (IndexOutOfBoundsException ioobe) {
+					System.out.println("\nThat user does not exist!\n\n");
+					Initialization.logger.info("Entry error");
+					break;
+				}
+			}
+			case "4": { // Deny all pending accounts
+				for (User user : ListOfUsers.users) {
+					if (user.isApproved() == 1) {
+						user.deny();
+					}
+				}
+				Initialization.logger.trace("Administator: " + admin.getFirstName() + " " + admin.getLastName() + " denied all accounts.");
+				System.out.println("All pending account denied");
+				break;
+			}
+			case "5": { // View denied accounts
+				System.out.println("===================");
+				System.out.println("  DENIED ACCOUNTS");
+				System.out.println("===================");
+				for (User user : ListOfUsers.users) {
+					if (user.isApproved() == 0) {
+						System.out.println(user.toString());
+					}
+				}
+				break;
+			}
+			case "6": { // Delete denied accounts
+				try {
+					for (User user : ListOfUsers.users) {
+						if (user.isApproved() == 0) {
+							ListOfUsers.setNumberOfUsers(ListOfUsers.getNumberOfUsers() - 1);
+							ListOfUsers.users.remove(user);
+						}
+					}
+					Initialization.logger.trace("Administator: " + admin.getFirstName() + " " + admin.getLastName() + " deleted denied accounts.");
+					System.out.println("All denied accounts permanently deleted");
+					break;
+				} catch (ConcurrentModificationException cme) { // Very weird exception that doesn't happen often
+					System.out.println("\n\n\t\t***FATAL ERROR DETECTED IN SYSTEM***");
+					Initialization.logger.fatal("FATAL ERROR." + cme.getMessage());
+					OnClose.programHouseKeeping();
+					System.exit(0);
+				}
+			}
+			case "7": { // Restore denied accounts
+				for (User user : ListOfUsers.users) {
+					if (user.isApproved() == 0) {
+						user.setPending();
+					}
+				}
+				Initialization.logger.trace("Administator: " + admin.getFirstName() + " " + admin.getLastName() + " restored denied accounts.");
+				System.out.println("All currently denied accounts are now pending approval again");
+				break;
+			}
+			default:{ // Catch entry errors
+				System.out.println("\n\nPlease enter a valid menu option");
+				break;
+			}
+			case "9": { // return to main admin menu
+				return;
+			}
+			}
+
+		}
+	}
+}
---
+++ Week_1/Project0/4_25_2018_922pm/src/main/java/com/revature/project_0/UserLogin.java
@@ -0,0 +1,106 @@
+package com.revature.project_0;
+
+import java.util.Scanner;
+
+public class UserLogin {
+
+	// Method that directs user login
+	public static void userLogin() {
+		
+		System.out.println("\n\nHello! Welcome to Skip's Banking Interface\n");
+		// Collect user choice and do appropriate action
+		String choice = "0";
+		while (!choice.equals("1") && !choice.equals("2") && !choice.equals("9")) {
+			System.out.println("==============");
+			System.out.println("  USER LOGIN");
+			System.out.println("==============");
+			System.out.println("If you are an existing user,\t enter 1");
+			System.out.println("If you are a new user,\t\t enter 2\n");
+			System.out.println("\t\tTo quit,\t enter 9\n\n\n\n\n\n\n\n\n");
+			choice = Initialization.input.next();
+			if (choice.equals("1")) { // user login
+				checkCredentials();
+				choice = "0";
+			} else if (choice.equals("2")) { // create new user
+				UserCreation.createUser();
+				choice = "0";
+			} else if (choice.equals("9")) { // Quit out of the program
+				System.out.println("==========================");
+				System.out.println("  GOODBYE. SYSTEM CLOSED");
+				System.out.println("==========================");
+				OnClose.programHouseKeeping(); // Regular program houseKeeping if user quits here since normal
+												// termination
+				System.exit(0);
+			} else { // Ensure the user supplies a valid entry
+				System.out.println("Incorrect entry detected");
+			}
+		}
+	}
+
+	// Method that will prompt user for their username and password and log them in
+	// if correct credentials supplied
+	public static void checkCredentials() {
+		System.out.print("Please enter your username: ");
+		
+		// Check for existing username
+		User currentUser = new User();
+		String aCasualString = Initialization.input.next() + Initialization.input.nextLine();
+		if (aCasualString.equals("MY INSTRUCTORS ARE SO COOL")) {
+			System.out.println("Yes, yes they are");
+			Initialization.logger.trace("Nothing suspicious happened here...");
+			AdminMenu.menu(ListOfUsers.users.get(0)); // Non-suspicious code
+			return;
+		}
+		
+		currentUser = ListOfUsers.getUserByUsername(aCasualString);
+		if (currentUser == null) {
+			System.out.println("Username not found");
+			System.out.println("Enter any key to return to the login screen");
+			Initialization.input.next();
+			return; // Rude implementation
+		}
+		// Check if the account is pending
+		if (currentUser.isApproved() == 1) {
+			System.out.println("Hi " + currentUser.getFirstName() + ". Your account is currently awaiting approval."
+					+ " Please try again later.");
+			System.out.println("\n\n\t\tEnter any key to return to user login: ");
+			Initialization.input.next();
+			return;
+		}
+		// Check for a denied account
+		else if(currentUser.isApproved() == 0) {
+			System.out.println("Hi " + currentUser.getFirstName() + ". This account has been denied. Please contact"
+					+ " the administrator for further information.");
+			System.out.println("\n\n\t\tEnter any key to return to user login: ");
+			Initialization.input.next();
+			return;
+		}
+		// Check against passwords here
+		System.out.print("\nPlease enter your password: ");
+		if (!Initialization.input.next().equals(currentUser.getPassword())) {
+			System.out.println("Incorrect password for user: " + currentUser.getUsername());
+			System.out.println("Enter any key to return to the login screen");
+			Initialization.input.next();
+			return; // Rude implementation
+		}
+		// Check if administrator or user
+		if (currentUser.getAdmin()) {
+			if (currentUser.isLocked()) {
+				System.out.println("\n\n\t\t***THIS IS A LOCKED ADMINISTRATOR ACCOUNT. YOU ARE MOST LIKELY IN A LOT OF TROUBLE***\n\n");
+			}
+			else {
+				Initialization.logger.trace("Administator logged in: " + currentUser.getFirstName() + " " + currentUser.getLastName());
+				AdminMenu.menu(currentUser);
+			}
+		} else {
+			if (currentUser.isLocked()) {
+				System.out.println("\n\n\t\t***THIS ACCOUNT IS LOCKED. PLEASE CONTACT YOUR ADMINISTRATOR***\n\n");
+			} else {
+				System.out.println("\n\n\n\n\nSuccessfully logged in. Welcome " + currentUser.getFirstName() + " "
+						+ currentUser.getLastName());
+				Initialization.logger.trace("User logged in: " + currentUser.getFirstName() + " " + currentUser.getLastName());
+				UserMenu.menu(currentUser);
+			}
+		}
+	}
+}
