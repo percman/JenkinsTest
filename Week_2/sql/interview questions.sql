354c301491049b2a650571ea5e502acd24abe528
andrew ahn <andrewsrahn@gmail.com>
2018-04-27 21:48:45 -0400

    modified interview questions.sql and

--- Week_2/sql/interview questions.sql
+++ Week_2/sql/interview questions.sql
@@ -1,4 +1,10 @@
 -- Write a query that will select the nth record in a table
--- Write a query that will delete duplicate entries in a table
+select name, mediatypeid from (select name, mediatypeid, rownum as rn from mediatype) where rn=3;
 
+-- Write a query that will delete duplicate entries in a table
+delete from mediatype
+    where rowid not in
+    (select min(rowid) 
+    from mediatype
+    group by mediatypeid, name);
 -- due friday 5pm, 27 April 2018
