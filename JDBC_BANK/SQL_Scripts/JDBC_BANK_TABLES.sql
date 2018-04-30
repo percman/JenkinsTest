f6978d1d4b78321dcdf3a00047bf52c6d7637194
aaalfaro <andyalfaro91@yahoo.com>
2018-04-30 10:17:42 -0400

    Updated sql table with up to date constraints

--- JDBC_BANK/SQL_Scripts/JDBC_BANK_TABLES.sql
+++ JDBC_BANK/SQL_Scripts/JDBC_BANK_TABLES.sql
@@ -10,27 +10,31 @@ CREATE TABLE credentials(
     CONSTRAINT PK_USER PRIMARY KEY(id)
     );
     
-
-    
 CREATE TABLE account (
     account_id NUMBER,
     username VARCHAR2(20),
-    funds FLOAT ,
+    funds FLOAT CHECK(funds >=0),
     CONSTRAINT PK_ACCOUNT PRIMARY KEY(account_id),
     CONSTRAINT FK_ACCOUNT FOREIGN KEY (username) REFERENCES credentials(username)
     ON DELETE CASCADE
     );
     
+    
 CREATE TABLE account_access(
     username VARCHAR2(20)UNIQUE,
-    locked VARCHAR2(5),
-    permissions VARCHAR2(5),
-    pending VARCHAR2(5),
+    locked VARCHAR2(5) DEFAULT 'false',
+    permissions VARCHAR2(5)DEFAULT 'false',
+    pending VARCHAR2(5)DEFAULT 'true',
     CONSTRAINT FK_ACCESS FOREIGN KEY (username) REFERENCES credentials(username)
     ON DELETE CASCADE
 );
+    
     
 --THIS IS THE ADMIN, needs to be in the db before using the program
 INSERT INTO credentials (username,password) VALUES('andy','boss');
 INSERT INTO account_access(username,permissions,pending) VALUES('andy','true','false');
 COMMIT;
+
+SELECT * FROM credentials;
+SELECT * FROM account_access;
+SELECT * FROM account;
