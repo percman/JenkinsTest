8ef2b8dc4f7c1d592224b438e0a1a00e9bc75906
adamjl217 <38381329+adamjl217@users.noreply.github.com>
2018-04-30 06:18:06 -0400

    having problems connecting, final commit here

--- JDBCBank/Project0test1.sql
+++ JDBCBank/Project0test1.sql
@@ -11,6 +11,11 @@ GRANT CREATE ANY PROCEDURE TO adminew;
 GRANT CREATE ANY TRIGGER TO adminew;
 GRANT CREATE ANY SEQUENCE TO adminew;
 GRANT UNLIMITED TABLESPACE TO adminew;
+GRANT ALTER ANY TABLE TO adminew;
+
+
+
+-- ******************************* TABLES *******************************
 
 -- create a user table 
 CREATE TABLE usertable (
@@ -26,6 +31,34 @@ CONSTRAINT BOOLEANLOCK CHECK (lockstatus BETWEEN 0 AND 1),
 CONSTRAINT BOOLEANADMIN CHECK (adminstatus BETWEEN 0 AND 1)
 );
 
+
+-- stores all user creation timestamp
+CREATE TABLE userrecord (
+id NUMBER(10),
+username VARCHAR2(16),
+time TIMESTAMP,
+CONSTRAINT PK_RECORD PRIMARY KEY (id)
+);
+
+
+-- junction table between usertable and userrecord
+--CREATE TABLE usertable_userrecord(
+--user_id NUMBER(10),
+--timelog TIMESTAMP,
+--CONSTRAINT UTUR_JUNCTION_TABLE PRIMARY KEY (user_id, timelog), 
+--CONSTRAINT FK_TIME FOREIGN KEY (timelog) REFERENCES userrecord (time) ON DELETE CASCADE, 
+--CONSTRAINT FK_USER FOREIGN KEY (user_id) REFERENCES usertable (id) ON DELETE CASCADE
+--);
+
+select * from usertable;
+
+
+
+
+
+-- ******************************* SEQUENCES *******************************
+
+
 -- create a sequence which will automatically increment the primary key
 CREATE SEQUENCE user_id_sequence 
     MINVALUE 0 
@@ -33,6 +66,20 @@ CREATE SEQUENCE user_id_sequence
     START WITH 1
     INCREMENT BY 1
     NOCACHE;
+
+
+-- id sequence for userrecord
+CREATE SEQUENCE record_id_sequence 
+    START WITH 1
+    INCREMENT BY 1
+    NOCACHE;
+
+
+
+
+
+-- ******************************* UDF'S *******************************
+
 
 -- hashing function that combines username, password, and a special word 
 CREATE OR REPLACE FUNCTION GET_USER_HASH(USERNAME VARCHAR2, PASSWORD VARCHAR2) RETURN VARCHAR2
@@ -44,56 +91,6 @@ BEGIN
 END;
 /
 
--- create a before insert trigger that will autoincrement the PK and hash the password 
-CREATE OR REPLACE TRIGGER user_b_insert 
-BEFORE INSERT 
-ON usertable
-FOR EACH ROW 
-BEGIN 
-    IF :new.id IS NULL THEN 
-        SELECT user_id_sequence.nextval INTO :new.id FROM dual;
-    END IF;
-    SELECT GET_USER_HASH(:new.username, :new.password) INTO :new.password FROM dual;
-END;
-/
-
--- procedure to insert new users 
-CREATE OR REPLACE PROCEDURE insert_user(new_username IN VARCHAR2, new_password IN VARCHAR2, 
-                                        new_balance IN NUMBER)
-AS 
-BEGIN
-    INSERT INTO usertable (username, password, lockstatus, adminstatus, balance, id)
-    VALUES (new_username, new_password, 1, 0, new_balance, null);
-    COMMIT;
-END;
-/
-
--- procedure to insert new admins 
-CREATE OR REPLACE PROCEDURE insert_admin(new_username IN VARCHAR2, new_password IN VARCHAR2, 
-                                        new_balance IN NUMBER)
-AS 
-BEGIN
-    INSERT INTO usertable (username, password, lockstatus, adminstatus, balance, id)
-    VALUES (new_username, new_password, 0, 1, new_balance, null);
-    COMMIT;
-END;
-/
-
--- update user procedure
-CREATE OR REPLACE PROCEDURE update_user(new_username IN VARCHAR2, new_password IN VARCHAR2, 
-                                        new_lock IN NUMBER, new_admin IN NUMBER,
-                                        new_balance IN NUMBER)
-AS 
-BEGIN
-    UPDATE usertable SET username = new_username, password = new_password, 
-    lockstatus = new_lock, balance = new_balance, adminstatus = new_admin
-    WHERE username = new_username;
-    COMMIT;
-END;
-/
-
-select * from usertable;
-UPDATE_USER('
 
 -- get balance UDF 
 CREATE OR REPLACE FUNCTION get_balance(curruser VARCHAR2, currpass VARCHAR2) RETURN NUMBER
@@ -114,6 +111,7 @@ BEGIN
 RETURN current_balance;
 END;
 /
+
 
 -- get lock status UDF 
 CREATE OR REPLACE FUNCTION get_lockstatus(curruser VARCHAR2, currpass VARCHAR2) RETURN NUMBER
@@ -156,38 +154,104 @@ RETURN current_admin;
 END;
 /
 
--- need one more sequence 
+
+-- gets the current time 
+CREATE OR REPLACE FUNCTION get_current_time RETURN TIMESTAMP 
+AS 
+    time_right_now TIMESTAMP; 
+BEGIN 
+    SELECT CURRENT_TIMESTAMP INTO time_right_now FROM dual;
+    dbms_output.put_line('The current time is ' || time_right_now);
+    RETURN time_right_now;
+END;
+/
 
 
--- stores all logon and logoff attempts by the admin
---CREATE TABLE userrecord (
---logon_logoff NUMBER(1),
---time TIMESTAMP,
---CONSTRAINT PK_RECORD PRIMARY KEY (time),
---CONSTRAINT LOGONBOOL CHECK (logon_logoff BETWEEN 0 AND 1)
---);
-
--- junction table between usertable and userrecord
---CREATE TABLE usertable_userrecord(
---user_id NUMBER(10),
---timelog TIMESTAMP,
---CONSTRAINT UTUR_JUNCTION_TABLE PRIMARY KEY (user_id, timelog), 
---CONSTRAINT FK_TIME FOREIGN KEY (timelog) REFERENCES userrecord (time) ON DELETE CASCADE, 
---CONSTRAINT FK_USER FOREIGN KEY (user_id) REFERENCES usertable (id) ON DELETE CASCADE
---);
-
--- finish this 
--- trigger to log the logon 
---CREATE OR REPLACE TRIGGER user_logon 
---AFTER LOGON 
---ON ProjectZero
---BEGIN 
---   INSERT INTO userrecord (id, timelog) VALUES (USER, CURRENT_TIMESTAMP);
---END;
---/
 
 
-select * from usertable;
+
+
+-- ******************************* TRIGGERS *******************************
+
+-- create a before insert for user trigger that will autoincrement the PK and hash the password 
+CREATE OR REPLACE TRIGGER user_b_insert 
+BEFORE INSERT 
+ON usertable
+FOR EACH ROW 
+BEGIN 
+    IF :new.id IS NULL THEN 
+        SELECT user_id_sequence.nextval INTO :new.id FROM dual;
+    END IF;
+    SELECT GET_USER_HASH(:new.username, :new.password) INTO :new.password FROM dual;
+END;
+/
+
+-- create a before insert for user trigger that will autoincrement the PK and hash the password 
+CREATE OR REPLACE TRIGGER record_b_insert 
+BEFORE INSERT 
+ON userrecord
+FOR EACH ROW 
+BEGIN 
+    IF :new.id IS NULL THEN 
+        SELECT record_id_sequence.nextval INTO :new.id FROM dual;
+    END IF;
+END;
+/
+
+-- stores initial time of initial user creation 
+CREATE OR REPLACE TRIGGER user_creation
+AFTER INSERT 
+ON usertable
+FOR EACH ROW
+BEGIN 
+   INSERT INTO userrecord (username, time) VALUES (:new.username, CURRENT_TIMESTAMP);
+END;
+/
+
+
+
+
+
+
+
+-- ******************************* PROCEDURES *******************************
+
+-- procedure to insert new users 
+CREATE OR REPLACE PROCEDURE insert_user(new_username IN VARCHAR2, new_password IN VARCHAR2, 
+                                        new_balance IN NUMBER)
+AS 
+BEGIN
+    INSERT INTO usertable (username, password, lockstatus, adminstatus, balance, id)
+    VALUES (new_username, new_password, 1, 0, new_balance, null);
+    COMMIT;
+END;
+/
+
+-- procedure to insert new admins 
+CREATE OR REPLACE PROCEDURE insert_admin(new_username IN VARCHAR2, new_password IN VARCHAR2, 
+                                        new_balance IN NUMBER)
+AS 
+BEGIN
+    INSERT INTO usertable (username, password, lockstatus, adminstatus, balance, id)
+    VALUES (new_username, new_password, 0, 1, new_balance, null);
+    COMMIT;
+END;
+/
+
+-- update user procedure
+CREATE OR REPLACE PROCEDURE update_user(new_username IN VARCHAR2, new_password IN VARCHAR2, 
+                                        new_lock IN NUMBER, new_admin IN NUMBER,
+                                        new_balance IN NUMBER)
+AS 
+BEGIN
+    UPDATE usertable SET username = new_username, password = new_password, 
+    lockstatus = new_lock, balance = new_balance, adminstatus = new_admin
+    WHERE username = new_username;
+    COMMIT;
+END;
+/
+
+
 
 
 
