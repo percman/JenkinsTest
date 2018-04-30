8107cc7d65d3e5db14b251daf08a8ef1717bce11
Andria <a.s.roberts@hotmail.com>
2018-04-30 04:29:14 -0400

    Uploading my project

--- JDBC_Bank/JDBC_Bank/src/main/java/com/bank/factory/AdminLogin.java
+++ JDBC_Bank/JDBC_Bank/src/main/java/com/bank/factory/AdminLogin.java
@@ -1,5 +1,21 @@
 package com.bank.factory;
 
+import java.util.Scanner;
+
+import com.bank.exception.UserNotFoundException;
+import com.bank.mainmenu.MainMenu;
+import com.bank.model.User;
+import com.bank.service.UserService;
+import com.bank.singleton.LogSingleton;
+
 public class AdminLogin {
 
+	public static void adminLogin(){
+		LogSingleton.getInstance();			
+			System.out.println("Welcome to the admin login!");		
+	
+		
+		
+//		MainMenu.startMenu();
+	}
 }
