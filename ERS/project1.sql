9cde7414eb299e6846228a74c104d24031cb873b
andrew ahn <andrewsrahn@gmail.com>
2018-05-10 00:34:23 -0400

    modified project1.sql

--- ERS/project1.sql
+++ ERS/project1.sql
@@ -1,12 +1,3 @@
---drop table managers cascade constraints purge;
---drop table employees cascade constraints purge;
---drop table information cascade constraints purge;
---drop table reimbursement cascade constraints purge;
---drop sequence pk_managers;
---drop sequence pk_employees;
---drop sequence pk_information;
---drop sequence pk_reimbursement;
-
 drop table employee cascade constraints purge;
 drop table manager cascade constraints purge;
 drop table information cascade constraints purge;
@@ -17,22 +8,22 @@ drop sequence pks_manager;
 drop sequence pks_reimbursement;
 
 create table employee(
-    employeeid number,
+    employeeid number not null,
     managerid number,
-    username varchar2(100) not null,
+    username varchar2(100) not null unique,
     password varchar2(100) not null,
     constraint pk_employeeid primary key(employeeid)
 );
 
 create table manager(
-    managerid number,
-    employeeid number,
+    managerid number not null,
+    employeeid number not null,
     constraint pk_managerid primary key(managerid),
     constraint fk__manager_employeeid foreign key(employeeid) references employee
 );
 
-alter table employee
-    add constraint fk_employee_managerid foreign key(managerid) references manager;
+--alter table employee
+--    add constraint fk_employee_managerid foreign key(managerid) references manager;
 
 create table information(
     informationid number,
@@ -47,11 +38,13 @@ create table information(
 create table reimbursement(
     reimbursementid number,
     employeeid number,
+    managerid number,
     status varchar2(100) not null,
     image varchar2(100),
     category varchar2(100),
     constraint pk_reimbursementid primary key(reimbursementid),
-    constraint fk_reimbursement_employeeid foreign key(employeeid) references employee(employeeid)
+    constraint fk_reimbursement_employeeid foreign key(employeeid) references employee(employeeid),
+    constraint fk_reimbursement_managerid foreign key(managerid) references manager(managerid)
 );
 
 create sequence pks_employee
@@ -104,6 +97,15 @@ begin
 end;
 /
 
+create or replace trigger before_update_employee
+before update
+on employee
+for each row
+begin
+    select hash(:new.username, :new.password) into :new.password from dual;
+end;
+/
+
 create or replace trigger before_create_information
 before insert
 on information
@@ -138,29 +140,34 @@ end;
 /
 
 create or replace procedure create_employee
-    (in_managerid number, in_username varchar2, in_password varchar2)
+    (in_username varchar2, in_password varchar2)
 as
 begin
     insert into employee(employeeid, managerid, username, password)
-        values(null, in_managerid, in_username, in_password);
+        values(null, null, in_username, in_password);
     commit;
 end;
 /
 
 create or replace procedure create_information
-    (in_employeeid number, in_firstname varchar2, in_middlename varchar2, in_lastname varchar2)
+    (in_username varchar2, in_firstname varchar2, in_middlename varchar2, in_lastname varchar2)
 as
+    in_employeeid number;
+    in_informationid number;
 begin
+    select employeeid into in_employeeid from employee where username=in_username;
     insert into information(informationid, employeeid, firstname, middlename, lastname)
-        values(null, in_employeeid, in_firstname, in_middlename, in_lastname);
+        values(in_informationid, in_employeeid, in_firstname, in_middlename, in_lastname);
     commit;
 end;
 /
 
 create or replace procedure create_manager
-    (in_employeeid number)
+    (in_username varchar2)
 as
+    in_employeeid number;
 begin
+    select employeeid into in_employeeid from employee where username=in_username;
     insert into manager(managerid, employeeid)
         values(null, in_employeeid);
     commit;
@@ -168,59 +175,74 @@ end;
 /
 
 create or replace procedure create_reimbursement
-    (in_employeeid number, in_status varchar2, in_image varchar2, in_category varchar2)
+    (in_username varchar2, in_status varchar2, in_image varchar2, in_category varchar2)
 as
+    in_employeeid number;
 begin
-    insert into reimbursement(reimbursementid, employeeid, status, image, category)
-        values(null, in_employeeid, in_status, in_image, in_category);
+    select employeeid into in_employeeid from employee where username=in_username;
+    insert into reimbursement(reimbursementid, employeeid, managerid, status, image, category)
+        values(null, in_employeeid, null, in_status, in_image, in_category);
     commit;
 end;
 /
 
 create or replace function read_employee
-    (in_employeeid number) return employee%rowtype
+    (in_username varchar2) return employee%rowtype
 as
+    in_employeeid number;
     return_row employee%rowtype;
 begin
+    select employeeid into in_employeeid from employee where username=in_username;
     select * into return_row from employee where employeeid=in_employeeid;
     return return_row;
 end;
 /
 
 create or replace function read_information
-    (in_informationid number) return information%rowtype
+    (in_username varchar2) return information%rowtype
 as
+    in_informationid number;
     return_row information%rowtype;
 begin
+    select i.informationid into in_informationid from information i, employee e
+        where i.employeeid=e.employeeid and e.username=in_username;
     select * into return_row from information where informationid=in_informationid;
     return return_row;
 end;
 /
 
 create or replace function read_manager
-    (in_managerid number) return manager%rowtype
+    (in_username varchar2) return manager%rowtype
 as
     return_row manager%rowtype;
 begin
-    select * into return_row from manager where managerid=in_managerid;
+    select * into return_row from manager where employeeid=
+        (select employeeid from employee where username=in_username);
     return return_row;
 end;
 /
 
 create or replace function read_reimbursement
-    (in_reimbursementid number) return reimbursement%rowtype
+    (in_username varchar2) return reimbursement%rowtype
 as
     return_row reimbursement%rowtype;
 begin
-    select * into return_row from reimbursement where reimbursementid=in_reimbursementid;
+    select * into return_row from reimbursement where reimbursementid=
+        (select r.reimbursementid from reimbursement r, employee e
+            where r.employeeid=e.employeeid and e.username=in_username);
     return return_row;
 end;
 /
 
 create or replace procedure update_employee
-    (in_employeeid number, in_managerid number, in_username varchar2, in_password varchar2)
+    (in_managerusername varchar2, in_username varchar2, in_password varchar2)
 as
+    in_employeeid number;
+    in_managerid number;
 begin
+    select employeeid into in_employeeid from employee where username=in_username;
+    select m.employeeid into in_managerid from manager m, employee e 
+        where m.employeeid=e.employeeid and e.username=in_managerusername;
     update employee 
         set managerid=in_managerid, username=in_username, password=in_password
         where employeeid=in_employeeid;
@@ -229,59 +251,135 @@ end;
 /
 
 create or replace procedure update_information
-    (in_informationid number, in_employeeid number, in_firstname varchar2, in_middlename varchar2, in_lastname varchar2)
+    (in_username varchar2, in_firstname varchar2, in_middlename varchar2, in_lastname varchar2)
 as
+    in_informationid number;
 begin
+    select informationid into in_informationid from information i, employee e
+        where i.employeeid=e.employeeid and username=in_username;
     update information
-        set employeeid=in_employeeid, firstname=in_firstname, middlename=in_middlename, lastname=in_lastname
+        set firstname=in_firstname, middlename=in_middlename, lastname=in_lastname
         where informationid=in_informationid;
     commit;
 end;
 /
 
 create or replace procedure update_reimbursement
-    (in_reimbursementid number, in_employeeid number, in_status varchar2, in_image varchar2, in_category varchar2)
+    (in_username varchar2, in_status varchar2, in_image varchar2, in_category varchar2)
 as
+    in_reimbursementid number;
 begin
+    select reimbursementid into in_reimbursementid from reimbursement r, employee e
+        where r.employeeid=e.employeeid and e.username=in_username;
     update reimbursement
-        set employeeid=in_employeeid, status=in_status, image=in_image, category=in_category
+        set status=in_status, image=in_image, category=in_category
         where reimbursementid=in_reimbursementid;
     commit;
 end;
 /
 
 create or replace procedure delete_employee
-    (in_employeeid number)
+    (in_username varchar2)
 as
 begin
-    delete from employee where employeeid=in_employeeid;
+    delete from employee where username=in_username;
     commit;
 end;
 /
 
 create or replace procedure delete_information
-    (in_informationid number)
+    (in_username varchar2)
 as
+    in_informationid number;
 begin
+    select informationid into in_informationid from information i, employee e 
+        where i.employeeid=e.employeeid and e.username=in_username; 
     delete from information where informationid=in_informationid;
     commit;
 end;
 /
 
 create or replace procedure delete_manager
-    (in_managerid number)
+    (in_username varchar2)
 as
+    in_employeeid number;
 begin
-    delete from manager where managerid=in_managerid;
+    select m.employeeid into in_employeeid from employee e, manager m 
+        where e.employeeid=m.employeeid and e.username=in_username; 
+    delete from manager where employeeid=in_employeeid;
     commit;
 end;
 /
 
 create or replace procedure delete_reimbursement
-    (in_reimbursementid number)
+    (in_username varchar2)
 as
+    in_reimbursementid number;
 begin
+    select r.reimbursementid into in_reimbursementid from employee e, reimbursement r
+        where e.employeeid=r.reimbursementid and e.username=in_username;
     delete from reimbursement where reimbursementid=in_reimbursementid;
     commit;
 end;
 /
+
+begin
+create_employee('andrew', 'password');
+create_employee('william', 'password');
+create_manager('william');
+create_information('william', 'william', 'oracle', 'gentry');
+create_information('andrew', 'andrew', 'ox', 'ahn');
+create_reimbursement('william', 'pending', 'http', 'lodging');
+create_reimbursement('andrew', 'approved', 'http', 'travel');
+update_employee('william', 'andrew', 'password');
+update_information('william', 'william', 'trainer', 'gentry');
+update_reimbursement('william', 'rejected', '...', 'lodging');
+commit;
+end;
+/
+
+select * from manager;
+select * from employee;
+select * from information;
+select * from reimbursement;
+
+begin
+delete_information('andrew');
+delete_information('william');
+delete_reimbursement('andrew');
+delete_reimbursement('william');
+delete_manager('william');
+delete_employee('william');
+delete_employee('andrew');
+commit;
+end;
+/
+
+select * from manager;
+select * from employee;
+select * from information;
+select * from reimbursement;
+
+/* 
+--begin
+--create_employee('william', 'password');
+--create_manager('william');
+--commit;
+--end;
+--/
+--select * from manager m, employee e where m.employeeid=e.employeeid and e.username='william';
+
+--begin
+--create_employee('william', 'password');
+--create_manager('william');
+--create_information('william', 'firstname', 'middlename', 'lastname');
+--commit;
+--end;
+--/
+--
+--select * from employee;
+--select * from manager;
+--select * from information;
+--
+--select * from information where employeeid=
+--    (select employeeid from employee where username='william');
